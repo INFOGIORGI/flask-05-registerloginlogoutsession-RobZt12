89b9b9d9db8dd6285b2b5ada739c241b0513a081
Roberto Marzio <103176073+RobZt12@users.noreply.github.com>
2025-01-10 12:39:18 +0000

    ciao

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def register():
+    return render_template("register.html")
+
+@app.route("/login") 
+def login():
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
+
+
 
 app.run()
