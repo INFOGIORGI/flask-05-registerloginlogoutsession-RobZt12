f86dd30ceecc2653d4108e71c967d002acef7896
Roberto Marzio <103176073+RobZt12@users.noreply.github.com>
2025-01-17 12:39:08 +0000

    marzio4

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,flash
 from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 app = Flask(__name__)
@@ -12,7 +13,7 @@ app.config['MYSQL_PASSWORD'] = 'ospite'
 app.config['MYSQL_DB'] = 'w3schools'
 
 mysql = MySQL(app)
-
+app.secret_key = "segereto"
 @app.route("/")
 def register():
     return render_template("register.html")
@@ -23,7 +24,7 @@ def login():
 
 @app.route("/logout")
 def logout():
-    return render_template("logout.html")
+    return render_template("login.html")
 
 @app.route("/area")
 def area():
@@ -33,16 +34,36 @@ def area():
 def registrati():
     if request.method == "GET":
         return render_template("register.html")
-    elif request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'nome' in request.form and 'cognome' in request.form and 'v_password' in request.form:
-        nome = request.form.get('nome')
-        cognome = request.form.get('cognome')
-        username = request.form.get('username')
-        password = request.form.get('password')
-        v_password = request.form.get('v_password')
-        cursor = mysql.connection.cursor()
-        query ="INSERT INTO users VALUES(%s,%s,%s,%s)"
-        cursor.execute(query,(nome,cognome,username,password))
-        mysql.connection.commit()
-        return redirect(url_for('area'))
+    
+    nome = request.form.get('nome')
+    cognome = request.form.get('cognome')
+    username = request.form.get('username')
+    password = request.form.get('password')
+    v_password = request.form.get('v_password')
 
+
+    for item in [nome, cognome, username, password, v_password]:
+        if item == "":
+            flash("Tutti i campi hannu a èssiri cumpilati")
+            return redirect("/register")
+        
+    cursor = mysql.connection.cursor()
+    query = "SELECT * FROM users WHERE username = %s"
+    cursor.execute(query,(username,))
+    result = cursor.fetchall()
+    if result:
+        flash("Username già prisenti, mo capisti?")
+        return redirect("/register")
+
+
+    cursor = mysql.connection.cursor()
+    query ="INSERT INTO users VALUES(%s,%s,%s,%s)"
+    cursor.execute(query,(username,generate_password_hash(password),nome,cognome))
+    """La flash() prende una serie di messaggi
+    e li inserisce all'interno di un vettore"""
+    mysql.connection.commit()
+    return redirect(url_for('area'))
+        
+      
+            
 app.run(debug=True)
