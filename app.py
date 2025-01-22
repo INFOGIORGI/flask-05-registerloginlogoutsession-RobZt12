7bec00b62a05399aa282e498c34fc62a58023be8
Roberto Marzio <103176073+RobZt12@users.noreply.github.com>
2025-01-22 22:31:21 +0000

    Marzio5

--- app.py
+++ app.py
@@ -5,64 +5,68 @@ from werkzeug.security import generate_password_hash,check_password_hash
 
 app = Flask(__name__)
 
+app.secret_key ='miao'
+
 
 app.config['MYSQL_HOST'] = '138.41.20.102'
 app.config['MYSQL_PORT'] = 53306
-app.config['MYSQL_USER'] = 'ospite'
-app.config['MYSQL_PASSWORD'] = 'ospite'
 app.config['MYSQL_DB'] = 'w3schools'
+app.config['MYSQL_USER'] = 'ospite'
+app.config['MYSQL_PASSWORD'] ='ospite'
 
-mysql = MySQL(app)
-app.secret_key = "segereto"
-@app.route("/")
+myslq = MySQL(app)
+
+@app.route('/',methods = ['POST','GET'])
 def register():
-    return render_template("register.html")
-
-@app.route("/login") 
+    if request.method == 'POST':
+        nome = request.form.get('nome','')
+        cognome = request.form.get('cognome','')
+        username = request.form.get('username','')
+        password = request.form.get('password','')
+        v_password = request.form.get('v_password','')
+        if nome == '' or cognome == '' or username == '' or password =='' or v_password =='':
+            flash('Inserire tutti i campi')
+            return redirect (url_for('register'))
+        else:
+            if password != v_password:
+                flash('Password non corrispondenti')
+                return redirect (url_for('register'))        
+            cursor = myslq.connection.cursor()
+            query_u = 'SELECT username FROM users WHERE username = %s'
+            cursor.execute(query_u,(username,))
+            result = cursor.fetchall()
+            if len(result) != 0:
+                flash('Username gia esiste')
+                return redirect (url_for('register'))
+            query = 'INSERT INTO users VALUES(%s,%s,%s,%s)'
+            cursor.execute(query,(username,generate_password_hash(password),nome,cognome))
+            myslq.connection.commit()
+            cursor.close()
+            return redirect (url_for('login'))
+    return render_template('register.html',titolo = 'Registrati')
+@app.route('/login',methods = ['POST','GET'])
 def login():
-    return render_template("login.html")
-
-@app.route("/logout")
-def logout():
-    return render_template("login.html")
-
-@app.route("/area")
-def area():
-    return render_template("area.html")
-
-@app.route("/register/",methods = ["GET","POST"])
-def registrati():
-    if request.method == "GET":
-        return render_template("register.html")
-    
-    nome = request.form.get('nome')
-    cognome = request.form.get('cognome')
-    username = request.form.get('username')
-    password = request.form.get('password')
-    v_password = request.form.get('v_password')
+    if request.method == 'POST':
+        username = request.form.get('username','')
+        password = request.form.get('password','')
+        cursor = myslq.connection.cursor()
+        query = 'SELECT password FROM users WHERE username = %s'
+        cursor.execute(query,(username,))
+        result = cursor.fetchall()
+        if len(result) == 0:
+            flash('Utente non esistente')
+            return redirect (url_for('register'))
+        else:
+            password_h = result[0][0]
+            if check_password_hash(password_h,password):
+                return render_template('area.html',titolo = 'AresPersonale')
+            else:
+                flash('Password non corrretta')
+                return redirect (url_for('login'))
+            
 
 
-    for item in [nome, cognome, username, password, v_password]:
-        if item == "":
-            flash("Tutti i campi hannu a èssiri cumpilati")
-            return redirect("/register")
-        
-    cursor = mysql.connection.cursor()
-    query = "SELECT * FROM users WHERE username = %s"
-    cursor.execute(query,(username,))
-    result = cursor.fetchall()
-    if result:
-        flash("Username già prisenti, mo capisti?")
-        return redirect("/register")
-
-
-    cursor = mysql.connection.cursor()
-    query ="INSERT INTO users VALUES(%s,%s,%s,%s)"
-    cursor.execute(query,(username,generate_password_hash(password),nome,cognome))
-    """La flash() prende una serie di messaggi
-    e li inserisce all'interno di un vettore"""
-    mysql.connection.commit()
-    return redirect(url_for('area'))
+    return render_template('login.html',titolo = 'Accedi')
         
       
             
